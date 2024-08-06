e5f317253b4f03f430806be14ba66552ee4fdbb8
opal1056 <vishwa.cool05@gmail.com>
2024-08-05 21:06:23 -0400

    Update uv.config.js

--- public/uv/uv.config.js
+++ public/uv/uv.config.js
@@ -1,7 +1,7 @@
 // This file overwrites the stock UV config.js
 
 self.__uv$config = {
-    prefix: "/uv/service/",
+    prefix: "/opal/",
     encodeUrl: Ultraviolet.codec.xor.encode,
     decodeUrl: Ultraviolet.codec.xor.decode,
     handler: "/uv/uv.handler.js",
